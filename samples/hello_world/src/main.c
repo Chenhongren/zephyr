c8d00f0df3dc5e7232febea064c35e8d38200050
Ren Chen <Ren.Chen@ite.com.tw>
2026-04-22 09:17:31 +0800

    test-only: intc-264/intc-265 test
    Change-Id: Ibef1d8052debb286992c7f5d56e694e565ad111e
    Signed-off-by: Ren Chen <Ren.Chen@ite.com.tw>

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,9 +6,42 @@
 
 #include <stdio.h>
 
+#include <zephyr/drivers/interrupt_controller/wuc_ite_it8xxx2.h>
+#include <zephyr/dt-bindings/interrupt-controller/it8xxx2-wuc.h>
+#include <zephyr/irq.h>
+#include <zephyr/dt-bindings/interrupt-controller/ite-it51xxx-intc.h>
+
+static void it51xxx_wu176_isr(const void *arg)
+{
+	printf("wu176 is triggered\n");
+	sys_write8(BIT(0), 0xf01b4d);
+}
+
+static void it51xxx_wu177_isr(const void *arg)
+{
+	printf("wu177 is triggered\n");
+	sys_write8(BIT(1), 0xf01b4d);
+}
+
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+
+	/* Connect WU90 (USB D+) interrupt but make it disabled initially */
+	irq_connect_dynamic(IT51XXX_IRQ_WU176, 0, it51xxx_wu176_isr, 0, 0);
+	irq_connect_dynamic(IT51XXX_IRQ_WU177, 0, it51xxx_wu177_isr, 0, 0);
+
+	/* write-1-clear wake-up interrupt status */
+	sys_write8(BIT(1) | BIT(0), 0xf01b4d);
+
+	/* enable usb device in gpiof4/gpiof5 */
+	sys_write8(BIT(1), 0xf016cf);
+
+	/* enable usb pull down */
+	sys_write8(BIT(5), 0xf016c2);
+
+	irq_enable(IT51XXX_IRQ_WU176);
+	irq_enable(IT51XXX_IRQ_WU177);
 
 	return 0;
 }
