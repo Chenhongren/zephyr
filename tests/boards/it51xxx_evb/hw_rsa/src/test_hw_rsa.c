66825949308991e4673dc330b8c44bef93a76f86
Ren Chen <Ren.Chen@ite.com.tw>
2026-09-15 14:03:10 +0800

    ite-internal: tests: boards: add it51xxx hw rsa test
    Tested with:
    - west build -p always -b it51xxx_evb/it51526aw \
      tests/boards/it51xxx_evb/hw_rsa/ \
      -T boards.it51xxx_evb.hw_rsa
    
    Change-Id: I8209be99f787fbc84a8c01a8269eae6c2a06be2b
    Signed-off-by: Ren Chen <Ren.Chen@ite.com.tw>

---
+++ tests/boards/it51xxx_evb/hw_rsa/src/test_hw_rsa.c
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2026 ITE Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/drivers/misc/it51xxx_hw_rsa/it51xxx_hw_rsa.h>
+#include <zephyr/kernel.h>
+#include <zephyr/ztest.h>
+
+#include "rsa_2048_keys.h"
+#include "rsa_4096_keys.h"
+
+ZTEST(it51xxx_hw_rsa, test_2048_key_bits)
+{
+	const bool exponent_3 = false;
+	struct device *hw_rsa_device = (struct device *)DEVICE_DT_GET(DT_NODELABEL(hw_rsa));
+	static struct it51xxx_hw_rsa_messages msgs;
+	static uint8_t output[sizeof(ciphertext_2048)];
+	struct it51xxx_hw_rsa_keys keys = {
+		.private_key = private_key_2048,
+		.private_key_sz = sizeof(private_key_2048),
+		.public_key = public_key_2048,
+		.public_key_sz = sizeof(public_key_2048),
+	};
+	int ret;
+
+	ret = it51xxx_hw_rsa_configure(hw_rsa_device, keys);
+	zassert_equal(ret, 0, "failed to configure keys");
+	msgs.input = plaintext_2048;
+	msgs.input_sz = sizeof(plaintext_2048);
+	msgs.output = output;
+	msgs.output_sz = sizeof(output);
+	memset(output, 0, sizeof(output));
+	ret = it51xxx_hw_rsa_encrypt(hw_rsa_device, &msgs);
+	zassert_equal(ret, 0, "failed to perform encryption");
+	ret = memcmp(msgs.output, ciphertext_2048, sizeof(ciphertext_2048));
+	zassert_equal(ret, 0, "encrypt error");
+
+	ret = it51xxx_hw_rsa_configure(hw_rsa_device, keys);
+	zassert_equal(ret, 0, "failed to configure keys");
+	msgs.input = ciphertext_2048;
+	msgs.input_sz = sizeof(ciphertext_2048);
+	msgs.output = output;
+	msgs.output_sz = sizeof(output);
+	memset(output, 0, sizeof(output));
+	ret = it51xxx_hw_rsa_decrypt(hw_rsa_device, exponent_3, &msgs);
+	zassert_equal(ret, 0, "failed to perform decryption");
+
+	ret = memcmp(msgs.output, plaintext_2048, sizeof(plaintext_2048));
+	zassert_equal(ret, 0, "decrypt error");
+}
+
+ZTEST(it51xxx_hw_rsa, test_4096_key_bits)
+{
+	const bool exponent_3 = false;
+	struct device *hw_rsa_device = (struct device *)DEVICE_DT_GET(DT_NODELABEL(hw_rsa));
+	static struct it51xxx_hw_rsa_messages msgs;
+	static uint8_t output[sizeof(ciphertext_4096)];
+	struct it51xxx_hw_rsa_keys keys = {
+		.private_key = private_key_4096,
+		.private_key_sz = sizeof(private_key_4096),
+		.public_key = public_key_4096,
+		.public_key_sz = sizeof(public_key_4096),
+	};
+	int ret;
+
+	ret = it51xxx_hw_rsa_configure(hw_rsa_device, keys);
+	zassert_equal(ret, 0, "failed to configure keys");
+	msgs.input = plaintext_4096;
+	msgs.input_sz = sizeof(plaintext_4096);
+	msgs.output = output;
+	msgs.output_sz = sizeof(output);
+	memset(output, 0, sizeof(output));
+	ret = it51xxx_hw_rsa_encrypt(hw_rsa_device, &msgs);
+	zassert_equal(ret, 0, "failed to perform encryption");
+	ret = memcmp(msgs.output, ciphertext_4096, sizeof(ciphertext_4096));
+	zassert_equal(ret, 0, "encrypt error");
+
+	ret = it51xxx_hw_rsa_configure(hw_rsa_device, keys);
+	zassert_equal(ret, 0, "failed to configure keys");
+	msgs.input = ciphertext_4096;
+	msgs.input_sz = sizeof(ciphertext_4096);
+	msgs.output = output;
+	msgs.output_sz = sizeof(output);
+	memset(output, 0, sizeof(output));
+	ret = it51xxx_hw_rsa_decrypt(hw_rsa_device, exponent_3, &msgs);
+	zassert_equal(ret, 0, "failed to perform encryption");
+	ret = memcmp(msgs.output, plaintext_4096, sizeof(plaintext_4096));
+	zassert_equal(ret, 0, "decrypt error");
+}
+
+ZTEST_SUITE(it51xxx_hw_rsa, NULL, NULL, NULL, NULL, NULL);
